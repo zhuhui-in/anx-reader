e9a621936656f11266513dd003a6e9ef9fcda87b
zhuhui-in <69226545+zhuhui-in@users.noreply.github.com>
2025-08-11 01:52:38 +0800

    Update prompt_generate.dart

--- lib/service/ai/prompt_generate.dart
+++ lib/service/ai/prompt_generate.dart
@@ -33,10 +33,10 @@ String generatePromptSummaryThePreviousContent(String previousContent) {
 String generatePromptTranslate(String text, String toLocale, String fromLocale) {
   String prompt = Prefs().getAiPrompt(AiPrompts.translate);
   final epubPlayerKey = GlobalKey<EpubPlayerState>();
-  final previousContent = epubPlayerKey.currentState!.previousContent(1000);  
+  String previousContent = Future.sync(() => epubPlayerKey.currentState!.previousContent(1000));
   prompt = prompt.replaceAll('{{text}}', text.trim());
   prompt = prompt.replaceAll('{{to_locale}}', toLocale);
   prompt = prompt.replaceAll('{{from_locale}}', fromLocale);
-  prompt = prompt.replaceAll('{{previous_content}}', previousContent);
+  prompt = prompt.replaceAll('{{previous_content}}', previousContent.trim());
   return prompt;
 }
